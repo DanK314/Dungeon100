62d5007f27239e1dce68a08d1234846f6d163323
DanK314 <kimdanoo.mnp@gmail.com>
2025-11-12 09:56:57 +0900

    Update game.js

--- game.js
+++ game.js
@@ -346,7 +346,7 @@ const GUN_SPECS = {
         bulletSpeed: 20,
         length: 20,
         fireRate: 1, // 0.001초 (레이저)
-        damage: 0.1, // 낮음
+        damage: 0.3, // 낮음
         type: 'railgun',
         name_kr: '레일건',
         desc_kr: '적을 관통하는 레이저를 쏩니다.',
